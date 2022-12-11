c5d515cbc647b94b4e54621a9813d382877577e4
shafakyildiz <shafakyildiz@gmail.com>
2022-12-11 23:33:16 +0300

    Add slug file

--- components/Post.js
+++ components/Post.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Post({ post }) {
   return (
-    <div class="card">
+    <div className="card">
       <img src={post.frontmatter.cover_image} alt="" />
       <div className="post-date">Posted on {post.frontmatter.date}</div>
       <h3>{post.frontmatter.title}</h3>
---
+++ pages/blog/[slug].js
@@ -0,0 +1,56 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import marked from "marked";
+import Link from "next/link";
+
+export default function PostPage({
+  frontmatter: { title, date, cover_image },
+  slug,
+  content,
+}) {
+  return (
+    <>
+      <Link href="/">
+        <a className="btn btn-back">Go Back</a>
+      </Link>
+      <div className="card card-page">
+        <h1 className="post-title">{title}</h1>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", "md"),
+    },
+  }));
+  console.log(paths);
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  console.log(slug);
+  const markdownWithMeta = fs.readFileSync(
+    path.join("posts", slug + ".md"),
+    "utf-8"
+  );
+
+  const { data: frontmatter, content } = matter(markdownWithMeta);
+
+  return {
+    props: {
+      frontmatter,
+      slug,
+    },
+  };
+}
