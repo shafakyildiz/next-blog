0d3ce136308ba1bdbc31ff50ba266308a25d51ad
shafakyildiz <shafakyildiz@gmail.com>
2022-12-11 22:09:59 +0300

    Remove css files and add header

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <Link href="/">
+          <h2>Dev Blog</h2>
+        </Link>
+      </div>
+    </header>
+  );
+}
